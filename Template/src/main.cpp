19c23ade099a9181bf9eb7bbeb762836faa79faa
danie <danie@DANIEL_L>
2026-01-28 18:16:38 -0500

    Add project files.

---
+++ Template/src/main.cpp
@@ -0,0 +1,10 @@
+﻿// CMakeTry.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
---
+++ Template/src/main.h
@@ -0,0 +1,14 @@
+﻿// CMakeTry.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "stb/stb_image.h"
+
+// TODO: Reference additional headers your program requires here.
