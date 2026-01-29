b431fa00aa38a4ebe70af733fcf03993c80d8e4e
MD12c <danielmilevsky@gmail.com>
2026-01-28 23:44:54 -0500

    added and linked graphics stuff

--- Template/src/main.cpp
+++ Template/src/main.cpp
@@ -1,10 +1,24 @@
-﻿// CMakeTry.cpp : Defines the entry point for the application.
-//
-
-#include "main.h"
+﻿#include "main.h"
+constexpr unsigned int width = 900;
+constexpr unsigned int height = 900;
 
 int main()
 {
 	std::cout << "Hello CMake." << std::endl;
+
+	// Name of the window, width & height of the window, background color RGB
+	Window VIEWPORT("Template", width, height, 0.7f, 0.7f, 0.7f);
+	VIEWPORT.glfwSetup();
+
+	Shader defShader("shaders/default.vert", "shaders/default.frag");
+
+	while (!glfwWindowShouldClose(VIEWPORT.getWindow())) {
+		VIEWPORT.glClearCurrentColor();
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(VIEWPORT.getWindow());
+		glfwPollEvents();
+	}
 	return 0;
 }
--- Template/src/main.h
+++ Template/src/main.h
@@ -3,12 +3,20 @@
 
 #pragma once
 
-#include <iostream>
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
-#include "stb/stb_image.h"
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<math.h>
+#include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<vector>
+
+#include"Graphics/Window.h"
+#include"Graphics/ShaderClass.h"
+#include"Graphics/VAO.h"
+#include"Graphics/VBO.h"
+#include"Graphics/EBO.h"
 
 // TODO: Reference additional headers your program requires here.
