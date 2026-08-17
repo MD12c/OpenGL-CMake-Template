1bbe1bf6e8560ab76427643e1bdeb20d818336c1
MD12c <danielmilevsky@gmail.com>
2026-08-17 14:37:22 -0400

    added square class

---
+++ Template/src/Graphics/FrameBuffers/Square.h
@@ -0,0 +1,22 @@
+#ifndef SQUARE_CLASS_H
+#define SQUARE_CLASS_H
+
+#include "../GLObjects/VAO.h"
+#include "../GLObjects/VBO.h"
+#include "../GLObjects/EBO.h"
+
+class Square
+{
+private:
+    VAO squareVAO;
+    VBO squareVBO;
+    EBO squareEBO;
+
+public:
+    Square();
+    void BindSquare();
+    void UnbindSquare();
+    void DrawSquare();
+};
+
+#endif
