015613a476ecbf17afa65fb9d15b46a3ea2764df
MD12c <danielmilevsky@gmail.com>
2026-08-28 18:00:31 -0400

    moved most of gl flag calls to rendere, added crntFrambuffer

--- Template/src/Graphics/Renderer.cpp
+++ Template/src/Graphics/Renderer.cpp
@@ -3,48 +3,123 @@
 #include "imgui.h"
 #include "Globals.h"
 #include "Cameras/Camera.h"
-#include "Lighting\LightSystem.h"
+#include "Framebuffers/Square.h"
 
 Renderer::Renderer()
-    : postProcessSystem(),
+    : antiAlias(),
+      finalFrameBuffer(2, true, "PostProcess"),
+      bloom(),
+      lut(),
       frustum(),
       noTexture(MaterialManager::makeTexture("Assets/Textures/noTexture.png", Texture::TextureType::ALBEDO))
 {
+    quad = new Square();
     ShaderManager::LoadAllShaders();
     Texture::setNoTextureID(noTexture->ID);
-    postProcessSystem.lut.Draw(ShaderID::BRDF_LUT);
+    for (int i = 0; i < RenderFlag::LAST_RENDERFLAG; i++)
+        applyFlag(flags[i], i);
+
+    lut.Draw(ShaderID::BRDF_LUT);
 }
 
 Renderer::~Renderer()
 {
+    delete quad;
     ShaderManager::PrintLoadedUniforms();
     ShaderManager::Cleanup();
 }
 
 void Renderer::Render(const Scene& scene)
 {
-    GPUInstrumentationTimer timer("Frame");
+    GPUInstrumentationTimer timerGPU("Frame");
+    InstrumentationTimer    timerCPU("Frame");
     frustum.setFrustumPlanes(scene.cameras[0]->getCameraMat());
     
-    scene.lightSystem.ShadowPass(scene.models, scene.lights, scene.worldTransform);
-
-    postProcessSystem.Begin();
+    set(DEPTH_b, true);
+    set(CULL_b, true);
+    set(CULL_FACE_c, GL_BACK);
+    glViewport(0, 0, ShadowCaster::SHADOW_MAP_WIDTH, ShadowCaster::SHADOW_MAP_HEIGHT);
+    scene.lightResources.ShadowPass(this, scene.models, scene.worldTransform);
+    
+    set(CULL_b, false);
+    set(DEPTH_FUNC_c, GL_LESS);
+    set(FRONT_FACE_c, GL_CCW);
+    glViewport(0, 0, width, height);
+    
+    BindFramebuffer(antiAlias.ID);
+    antiAlias.MSAAbufferRBO.Bind();
+    glClear(GL_DEPTH_BUFFER_BIT);
 
     auto useShader = ShaderID::PBR;
-    scene.lightSystem.ExportUniforms(useShader, scene.lights);
+    scene.lightResources.ExportUniformsTo(useShader);
     scene.skybox.ExportUniformsTo(useShader);
-    postProcessSystem.lut.ExportUniformsTo(useShader);
-
+    lut.ExportUniformsTo(useShader);
     scene.cameras[scene.activeCam]->updateUniforms(useShader);
     for (const auto& model : scene.models)
         model.Draw(useShader, scene.worldTransform, &frustum);
 
     scene.cameras[scene.activeCam]->updateUniforms(ShaderID::LIGHT_SPHERE);
-    scene.lightSystem.DrawLightSpheres(ShaderID::LIGHT_SPHERE, scene.lights);
+    scene.lightResources.DrawLightSpheres(ShaderID::LIGHT_SPHERE);
 
+    set(CULL_b, false);
+    set(DEPTH_FUNC_c, GL_LEQUAL);
     scene.skybox.Draw(ShaderID::SKYBOX, scene.cameras[scene.activeCam]->getRotationMat());
+    set(DEPTH_FUNC_c, GL_LESS);
+    set(CULL_b, true);
 
-    postProcessSystem.End();
+    antiAlias.CopyResultsTo(finalFrameBuffer);
+    Texture& blurredTexture = bloom.BlurPass(finalFrameBuffer.textures[1], ShaderID::BLUR, 3);
 
+    ShaderManager::Activate(ShaderID::POSTPROCESS);
+    glUniform1f(ShaderManager::getLoc(ShaderID::POSTPROCESS, "gamma"), gamma);
+    finalFrameBuffer.textures[0].texUnit(ShaderID::POSTPROCESS, "tex0");
+    blurredTexture.texUnit(ShaderID::POSTPROCESS, "tex1");
+    
+    BindFramebuffer(0);
+    glViewport(0, 0, width, height);
+    set(DEPTH_b, false);
+    quad->DrawSquare();
+    glClear(GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    set(DEPTH_b, true);
+    Texture::UnbindAll();
+    CubeTexture::UnbindAll();
+    // std::cout << glGetError() << std::endl;
     scene.imguiFunctions();
+}
+
+void Renderer::set(RenderFlag f, GLenum value)
+{
+    if (flags[f] == value)
+        return;
+    flags[f] = value;
+
+    switch (f)
+    {
+        case DEPTH_b:
+            applyFlag(value, GL_DEPTH_TEST);
+            break;
+        case STENCIL_b:
+            applyFlag(value, GL_STENCIL_TEST);
+            break;
+        case COLOR_b:
+            applyFlag(value, GL_COLOR_LOGIC_OP);
+            break;
+        case CULL_b:
+            applyFlag(value, GL_CULL_FACE);
+            break;
+        case CULL_FACE_c:
+            glCullFace(value);
+            break;
+        case FRONT_FACE_c:
+            glFrontFace(value);
+            break;
+        case DEPTH_FUNC_c:
+            glDepthFunc(value);
+            break;
+        case MULTISAMPLE_b:
+            applyFlag(value, GL_MULTISAMPLE);
+            break;
+        default:
+            break;
+    }
 }
--- Template/src/Graphics/Renderer.h
+++ Template/src/Graphics/Renderer.h
@@ -3,21 +3,51 @@
 
 #include "Scene.h"
 #include "Frustum/Frustum.h"
-#include "FrameBuffers/PostProcessSystem.h"
+#include "FrameBuffers/MSAABuffer.h"
+#include "FrameBuffers/Framebuffer.h"
+#include "FrameBuffers/Bloom.h"
+#include "FrameBuffers/LUT.h"
 #include "Models/Transform.h"
+
+// _b bool, _c choice
+enum RenderFlag
+{
+    DEPTH_b,
+    DEPTH_FUNC_c,
+    STENCIL_b,
+    COLOR_b,
+    CULL_b,
+    CULL_FACE_c,
+    FRONT_FACE_c,
+    MULTISAMPLE_b,
+    LAST_RENDERFLAG
+};
 
 class Renderer
 {
 public:
-    PostProcessSystem postProcessSystem;
-    Frustum           frustum;
+    std::array<GLenum, RenderFlag::LAST_RENDERFLAG> flags = {};
+
+    MSAAbuffer   antiAlias;
+    Framebuffer  finalFrameBuffer;
+    Bloom        bloom;
+    LUT          lut;
+    Frustum      frustum;
 
     std::shared_ptr<Texture> noTexture;
+
+    GLuint boundFramebuffer = 0;
+    void   BindFramebuffer(GLenum ID) { ID != boundFramebuffer ? glBindFramebuffer(GL_FRAMEBUFFER, boundFramebuffer = ID) : void(); }
 
     Renderer();
     ~Renderer();
 
     void Render(const Scene& scene);
+
+private:
+    void        set(RenderFlag f, GLenum value);
+    bool        isSet(RenderFlag f) const { return flags[f]; }
+    static void applyFlag(bool apply, GLenum target) { apply ? glEnable(target) : glDisable(target); }
 };
 
 #endif
